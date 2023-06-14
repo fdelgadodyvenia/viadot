33420ec539a35083e3b2848023850d4ea13617d7
cgildenia <cgildenia@dyvenia.com>
2023-06-14 14:58:48 +0200

    🐛 fixed task test

--- tests/integration/tasks/test_vid_club.py
+++ tests/integration/tasks/test_vid_club.py
@@ -1,7 +1,10 @@
-import pytest
 from unittest import mock
+
 import pandas as pd
-from viadot.tasks.vid_club import VidClubToDF
+import pytest
+
+from viadot.tasks import VidClubToDF
+
 
 class MockVidClubResponse:
     response_data = pd.DataFrame()
@@ -12,8 +15,11 @@ def var_dictionary():
     variables = {"source": "jobs", "from_date": "2022-03-23", "to_date": "2022-03-24"}
     yield variables
 
-@mock.patch.object("VidClubToDF.run", mock_run=MockVidClubResponse.response_data)
-def test_vid_club_to_df(mock_run, var_dictionary):
+
+@mock.patch(
+    "viadot.tasks.VidClubToDF.run", return_value=MockVidClubResponse.response_data
+)
+def test_vid_club_to_df(var_dictionary):
     source = var_dictionary["source"]
     from_date = var_dictionary["from_date"]
     to_date = var_dictionary["to_date"]
